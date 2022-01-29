e1c1de369739c4175a9980a43b094cbcfaca7149
DeliaFM <delia.mustelierova@gmail.com>
2022-01-29 22:45:32 +0100

    Completed counter

---
+++ script.js
@@ -0,0 +1,28 @@
+$('.CountTitle').text('Counter');
+
+var num = 0;
+$('.CountNum').text(num);
+
+$("#incrBtn").click(function () {
+    $(".countNum").val(++num);
+    $('.CountNum').text(num);
+    
+});
+
+$("#decrBtn").click(function () {
+    if (num !== 0) {
+        $(".countNum").val(--num);
+        $('.CountNum').text(num);
+    };
+    
+});
+
+
+
+/* 
+Replicate the basic HTML above (2 different headings and a button)
+Create an EventListener for clicking on the button, increase the value of the second heading (currently 0)
+(Optional) Create a working decreasing button as well
+(Hint) Any content of an HTML element is going to be a string
+(Hint) You can store the number in a variable
+*/
